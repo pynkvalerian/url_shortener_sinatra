13b80450a9ed77020d3259c27c99168b3549b3f4
Jay C <imjching@hotmail.com>
2015-08-26 10:56:32 +0800

    Implement a working url shortening function.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,21 @@
 get '/' do
   # let user create new short URL, display a list of shortened URLs
+erb :index
 end
 
 post '/urls' do
   # create a new Url
+
+  @url = params[:url]
+  @new_link = Url.create(url: @url, shortened_url: Url.generate_short_url, click_count: 0)
+  erb :short
 end
+
 
 # e.g., /q6bda
 get '/:short_url' do
   # redirect to appropriate "long" URL
+  @url = Url.get_url(params[:short_url])
+  byebug
+  redirect to("http://" + @url)
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,17 @@
 class Url < ActiveRecord::Base
   # Remember to create a migration!
+  # attr_accessor :url, :shortened_url, :click_count
+  @root = 'http://localhost:9393/'
+  def self.generate_short_url
+    @shortened_url = ('a'..'z').to_a.shuffle[0,5].join
+  end
+
+  def self.get_url(short_url)
+    link = self.find_by shortened_url: short_url
+    link.click_count = link.click_count + 1
+    link.save
+    byebug
+    link.url
+  end
+
 end
